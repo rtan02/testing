ecd31730b75097d764687ee3a23a898852a526d4
rtan02 <59622300+rtan02@users.noreply.github.com>
2020-01-14 13:50:40 -0500

    spacing
    changed spacing and

--- src/TestClassroom.java
+++ src/TestClassroom.java
@@ -6,18 +6,22 @@ public class TestClassroom {
     
     public static void main(String[] args) {
         
-        Classroom[] stud = new Classroom[1];
+        Classroom[] stud = new Classroom[5];
         
         Scanner scan = new Scanner(System.in);
         
         for (int i = 0; i < stud.length; i++) {
             stud[i] = new Classroom();
+            
             System.out.println("Enter Id: ");
             stud[i].setId(scan.nextInt());
+            
             System.out.println("Enter Name: ");
             stud[i].setName(scan.next());
+            
             System.out.println("Enter Address: ");
             stud[i].setAddress(scan.next());
+            
             System.out.println("Enter Program Name: ");
             stud[i].setProgramName(scan.next());
         }
